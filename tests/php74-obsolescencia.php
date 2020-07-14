fcbb6bcbea783112c1f48cf28dfe08a5cd24a4d0
Nawarian <nickolas@phpsp.org.br>
2020-07-14 23:41:16 +0200

    Adiciona teste sobre real types

--- tests/php74-obsolescencia.php
+++ tests/php74-obsolescencia.php
@@ -4,4 +4,19 @@ use PHPUnit\Framework\TestCase;
 
 class TestPHP74Obsolescencia extends TestCase
 {
+    /**
+     * Fazer o cast
+     * @test
+     * @group php74
+     */
+    public function realType(): void
+    {
+        $x = (real) 10;
+
+        self::assertEquals('The (real) cast is deprecated, use (float) instead', error_get_last()['message']);
+        self::assertSame(10.0, $x);
+
+        self::assertTrue(is_real($x));
+        self::assertEquals('Function is_real() is deprecated', error_get_last()['message']);
+    }
 }
