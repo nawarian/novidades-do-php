ff0540c2ead14df186ea2bf5fce5436b6f6bdf79
Nawarian <nickolas@phpsp.org.br>
2020-07-14 23:44:40 +0200

    Adiciona teste sobre magic quotes

--- tests/php74-obsolescencia.php
+++ tests/php74-obsolescencia.php
@@ -19,4 +19,21 @@ class TestPHP74Obsolescencia extends TestCase
         self::assertTrue(is_real($x));
         self::assertEquals('Function is_real() is deprecated', error_get_last()['message']);
     }
+
+    /**
+     * @test
+     * @group php74
+     */
+    public function magicQuotes(): void
+    {
+        /**
+         * As duas funções abaixo foram marcadas como obsoletas
+         * e passaram a lançar warnings.
+         */
+        self::assertFalse(get_magic_quotes_gpc());
+        self::assertEquals('Function get_magic_quotes_gpc() is deprecated', error_get_last()['message']);
+
+        self::assertFalse(get_magic_quotes_runtime());
+        self::assertEquals('Function get_magic_quotes_runtime() is deprecated', error_get_last()['message']);
+    }
 }
